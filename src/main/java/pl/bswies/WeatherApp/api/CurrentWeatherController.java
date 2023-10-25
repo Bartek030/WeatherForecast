ce9df7733dce58244ea98124f17dbc8e3bd85d0e
Bartlomiej Swies <bartlomiejswies@gmail.com>
2023-10-25 23:41:41 +0200

    Swagger configuration

--- src/main/java/pl/bswies/WeatherApp/api/CurrentWeatherController.java
+++ src/main/java/pl/bswies/WeatherApp/api/CurrentWeatherController.java
@@ -1,6 +1,13 @@
 package pl.bswies.WeatherApp.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.bswies.WeatherApp.business.models.WeatherData;
@@ -15,10 +22,30 @@ public class CurrentWeatherController {
 
     private final CurrentWeatherService currentWeatherService;
 
+    @Operation(summary = "Get Current Weather Data for specified city")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Current weather data was found",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(
+                                            implementation = WeatherData.class
+                                    )
+                            )
+                    }),
+            @ApiResponse(
+                    responseCode = "503",
+                    description = "Server unavailable",
+                    content = @Content)
+    })
     @GetMapping
     public ResponseEntity<WeatherData> getCurrentWeatherData(
             @PathVariable String cityName,
+            @Parameter(description = "standerd, metric or imperial")
             @RequestParam String units,
+            @Parameter(description = "Language in short form - for example: en")
             @RequestParam String lang
     ) {
         WeatherData weatherData = currentWeatherService.getCurrentWeatherData(cityName, units, lang);
---
+++ src/main/java/pl/bswies/WeatherApp/config/SpringDocConfiguration.java
@@ -0,0 +1,39 @@
+package pl.bswies.WeatherApp.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.bswies.WeatherApp.WeatherAppApplication;
+
+@Configuration
+public class SpringDocConfiguration {
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+            .group("default")
+            .pathsToMatch("/**")
+            .packagesToScan(WeatherAppApplication.class.getPackageName())
+            .build();
+    }
+
+    @Bean
+    public OpenAPI springDocOpenApi() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(new Info()
+                .title("Weather application")
+                .contact(contact())
+                .version("1.0"));
+    }
+
+    private Contact contact() {
+        return new Contact()
+            .name("Bartek")
+            .email("bartlomiejswies@gmail.com");
+    }
+}
